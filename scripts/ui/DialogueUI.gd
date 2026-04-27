354b7651e9f02eecd946d48ea0d4d4311f162af1
Sheetal Kadam <sheetalkadam@Sheetals-MacBook-Pro.local>
2026-04-26 20:17:05 -0700

    feat: implement core dialogue engine with inner monologue support

---
+++ scripts/ui/DialogueUI.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var spoken_text: RichTextLabel = $MainBox/SpokenText
+@onready var speaker_label: Label = $MainBox/SpeakerLabel
+@onready var inner_monologue: RichTextLabel = $InnerMonologue
+
+func _ready() -> void:
+	visible = false
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+	DialogueManager.line_presented.connect(_on_line_presented)
+
+func _on_dialogue_started(_id: String) -> void:
+	visible = true
+
+func _on_dialogue_ended() -> void:
+	visible = false
+
+func _on_line_presented(line_data: Dictionary) -> void:
+	speaker_label.text = line_data.get("speaker", "???")
+	spoken_text.text = line_data.get("text", "")
+	
+	var mono = line_data.get("monologue", "")
+	if mono != "":
+		inner_monologue.text = "[i]%s[/i]" % mono
+		inner_monologue.visible = true
+	else:
+		inner_monologue.visible = false
